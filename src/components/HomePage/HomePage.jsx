420e920efbdfcf448fcce52509c9ab71948bbc06
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-19 20:56:17 +0600

    feat(services): create services section to show our services

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "./Banner";
+import Services from "./Services";
 
 const HomePage = () => {
   return (
     <div>
       <Banner></Banner>
+      <Services></Services>
     </div>
   );
 };
