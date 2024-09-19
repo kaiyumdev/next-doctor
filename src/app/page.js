8ba835ead5939f0bd6350a2c23f38b8b0918cb0c
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-19 20:48:01 +0600

    feat(banner): implements banner at home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
-import Banner from "@/components/HomePage/Banner";
 import HomePage from "@/components/HomePage/HomePage";
 
 export default function Home() {
   return (
     <main>
       <HomePage></HomePage>
-      <Banner></Banner>
     </main>
   );
 }
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Banner from "./Banner";
 
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Banner></Banner>
+    </div>
+  );
 };
 
 export default HomePage;
