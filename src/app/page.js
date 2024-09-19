c7fd1f926212cb24d34dbf69d10850ab2c315af0
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-19 20:43:12 +0600

    feat(banner): create banner page for next-doctor project

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Banner from "@/components/HomePage/Banner";
 import HomePage from "@/components/HomePage/HomePage";
 
 export default function Home() {
   return (
     <main>
       <HomePage></HomePage>
+      <Banner></Banner>
     </main>
   );
 }
