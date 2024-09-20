58dbffcfdd7846cf1524317a6fd395c27f6210f2
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-20 18:40:51 +0600

    feat(credentials): implements credentials at auth route for check authenticate users

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,7 @@
+import { connectDB } from "@/lib/connectDB";
 import NextAuth from "next-auth/next";
 import credentialsProvider from "next-auth/providers/credentials";
+import { bcrypt } from "bcrypt";
 
 const handler = NextAuth({
   session: {
@@ -13,7 +15,23 @@ const handler = NextAuth({
         password: {},
       },
       async authorize(credentials) {
-        return true;
+        const { email, password } = credentials;
+        if (!email || !password) {
+          return null;
+        }
+        const db = await connectDB();
+        const currentUser = await db.collection("users").findOne({ email });
+        if (!currentUser) {
+          return null;
+        }
+        const passwordMatched = bcrypt.compareSync(
+          password,
+          currentUser.password
+        );
+        if (!passwordMatched) {
+          return null;
+        }
+        return currentUser;
       },
     }),
   ],
