aba103cdd972daf2badb62c2af770a38e293933a
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-22 01:29:02 +0600

    feat(patch): create patch route to update bookings route

--- src/app/my-bookings/api/booking/[id]/route.js
+++ src/app/my-bookings/api/booking/[id]/route.js
@@ -16,6 +16,31 @@ export const DELETE = async (request, { params }) => {
   }
 };
 
+export const PATCH = async (request, { params }) => {
+  const db = await connectDB();
+  const bookingsCollection = db.collection("bookings");
+  const updateDoc = await request.json();
+  try {
+    const resp = await bookingsCollection.updateOne(
+      { _id: new ObjectId(params.id) },
+      {
+        $set: {
+          ...updateDoc,
+        },
+      },
+      {
+        upsert: true,
+      }
+    );
+    return NextResponse.json({
+      message: "updated the booking",
+      response: resp,
+    });
+  } catch (error) {
+    return NextResponse.json({ message: "Something Went Wrong" });
+  }
+};
+
 export const GET = async (request, { params }) => {
   const db = await connectDB();
   const bookingsCollection = db.collection("bookings");
