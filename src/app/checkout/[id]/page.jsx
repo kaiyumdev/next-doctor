13556117827e28cef71a57424ffe8740f832d948
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-21 18:58:17 +0600

    feat(checkoutForm): create checkout form to fillup the checkout submission

--- src/app/checkout/[id]/page.jsx
+++ src/app/checkout/[id]/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import { getServicesDetails } from "@/services/getServices";
 import Image from "next/image";
 import React from "react";
@@ -6,9 +7,11 @@ const page = async ({ params }) => {
   const details = await getServicesDetails(params.id);
   // Destructure the details to extract img, title, description, etc.
   const { img, title, description, facility, price, _id } = details.service;
+  const handleBooking = () => {};
   return (
     <div className="container mx-auto">
-      <div className="relative h-72">
+      {/* <ToastContainer /> */}
+      <div className="relative  h-72">
         <Image
           className="absolute h-72 w-full left-0 top-0 object-cover"
           src={img}
@@ -19,9 +22,91 @@ const page = async ({ params }) => {
         />
         <div className="absolute h-full left-0 top-0 flex items-center justify-center bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] ">
           <h1 className="text-white text-3xl font-bold flex justify-center items-center ml-8">
-            checkout {title}
+            Checkout {title}
           </h1>
         </div>
+      </div>
+      <div className="my-12 bg-slate-300 p-12">
+        <form onSubmit={handleBooking}>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                // defaultValue={data?.user?.name}
+                type="text"
+                name="name"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Date</span>
+              </label>
+              <input
+                // defaultValue={new Date().getDate()}
+                type="date"
+                name="date"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                // defaultValue={data?.user?.email}
+                type="text"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Due amount</span>
+              </label>
+              <input
+                // defaultValue={price}
+                readOnly
+                type="text"
+                name="price"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Phone</span>
+              </label>
+              <input
+                required
+                type="text"
+                name="phone"
+                placeholder="Your Phone"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Present Address</span>
+              </label>
+              <input
+                type="text"
+                name="address"
+                placeholder="Your Address"
+                className="input input-bordered"
+              />
+            </div>
+          </div>
+          <div className="form-control mt-6">
+            <input
+              className="btn btn-primary btn-block"
+              type="submit"
+              value="Order Confirm"
+            />
+          </div>
+        </form>
       </div>
     </div>
   );
