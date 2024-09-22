7d1aa290c5b1c75ff31ef5b9bc86bd11da48b344
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-22 12:28:14 +0600

    Feat(url): use localhost url dynamically

--- src/services/getServices.js
+++ src/services/getServices.js
@@ -1,11 +1,15 @@
 export const getServices = async () => {
-  const res = await fetch("http://localhost:3000/services/api/get-all");
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/services/api/get-all`
+  );
   const services = res.json();
   return services;
 };
 
 export const getServicesDetails = async (id) => {
-  const res = await fetch(`http://localhost:3000/services/api/${id}`);
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/services/api/${id}`
+  );
   const service = res.json();
   return service;
 };
